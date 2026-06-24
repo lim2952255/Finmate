ed23c98171ffedf2aa7530d4786e3fedf2c30249
limhs2255 <limhs2255@limhs2255ui-MacBookAir.local>
2026-06-24 09:39:12 +0900

    Initial project setup

---
+++ src/main/java/com/finmate/FinmateApplication.java
@@ -0,0 +1,13 @@
+package com.finmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finmate/FinmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
